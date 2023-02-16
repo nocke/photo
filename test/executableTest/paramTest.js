091f97bfe545ffbccb6532e1a52fed6c87c50715
_USER_ <_@_>
2023-02-16 20:51:40 +0100

    fix paramTest

--- test/executableTest/paramTest.js
+++ test/executableTest/paramTest.js
@@ -1,29 +1,24 @@
 import { assert } from 'chai'
-import { check, guard } from '@nocke/util'
+import { check, guard, warn } from '@nocke/util'
 
 /*
-Testing, if parameter calling (the wirings of Commander) is halfway reasonable
+  Testing by indeed calling the command on the test folder:
  */
-
 
 describe(autoSuiteName(
   import.meta.url),
 () => {
-
   it('help string sanity test', () => {
     const r = guard('./photo --help', { mute: true })
-    assert(r.includes('Usage:', 'help does not contain \'Usage:\''), 'help string sanity test')
+    assert(r.includes('Usage:'), 'help does not contain \'Usage:\'')
   })
 
-  it.skip /* TODO */('regular sanitize run', () => {
-    // yes, this may run on the ORIG folder, as I am not going live...
-    const r = guard('./photo -v sanitize ../../../common/images/TEST-FOLDER', { mute: true })
-    assert(r.includes('Usage:', 'help does not contain \'Usage:\''), 'help string sanity test')
+  it('regular sanitize run', () => {
+    const r = guard('./photo -v sanitize ./TEST-FOLDER', { mute: true })
+    assert(r.includes('stats'), 'no stats mentioned')
   })
-
 
   // negative testing
-
   it('nonsense command must not return 0', () => {
     // since there's an optional command (sanitize),
     // totalnonsense will be regarded a directory and fail _then_ ...
