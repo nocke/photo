e1f649469c1a3aa6a4bdbca8e5339d734397892b
_USER_ <_@_>
2023-02-27 09:55:02 +0100

    phowdy

--- script/phowdy.js
+++ script/phowdy.js
@@ -3,8 +3,19 @@
 import '../src/config.js'
 import { info, mainWrap, sleep, sleepWithKeypress, warn } from '@nocke/util'
 
-info('Start')
-const c = await sleepWithKeypress(1000)
+// info('Start')
+// const c = await sleepWithKeypress(1000)
 
-warn('Done', c, c === '')
-if (c === '') await sleep(1000)
+// warn('Done', c, c === '')
+// if (c === '') await sleep(1000)
+
+// console.log('Oins')
+// console.log('Zwoi')
+// // console.log(`\u001b[1A\u001b[2m`)
+// process?.stdout.write('\x1b[1A\x1b[2K')
+// console.log('Drü')
+
+console.log(`\x1b[90mBanane\x1b[0m`)
+console.log('\x1b[90mBanane\x1b[0m')
+info('banane')
+warn('banane')
